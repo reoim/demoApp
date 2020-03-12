98eebac0cb41b97cdc488f76d19440c3c452aa54
Lee <reolee@a483e7813af1.ant.amazon.com>
2020-03-12 18:02:04 +0900

    CRUD for backend

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.14
 require (
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.1.15
-	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 )
--- handlers/tasks.go
+++ handlers/tasks.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/reoim/demoApp/models"
 	"github.com/labstack/echo/v4"
 )
 
@@ -13,13 +14,23 @@ type H map[string]interface{}
 // GetTasks endpoint
 func GetTasks(db *sql.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.JSON(http.StatusOK, "tasks")
+		result := models.GetTasks(db)
+		return c.JSON(http.StatusOK, result)
 	}
 }
+
+
 
 // PutTask endpoint
 func PutTask(db *sql.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
+
+		t := new(models.Task)
+		if err := c.Bind(t); err != nil {
+			return err
+		}
+		models.PutTask(db, t.Name)
+
 		return c.JSON(http.StatusCreated, H{
 			"created": 123,
 		})
@@ -30,6 +41,7 @@ func PutTask(db *sql.DB) echo.HandlerFunc {
 func DeleteTask(db *sql.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		id, _ := strconv.Atoi(c.Param("id"))
+		models.DeleteTask(db, id)
 		return c.JSON(http.StatusOK, H{
 			"deleted": id,
 		})
